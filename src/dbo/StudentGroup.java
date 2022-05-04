08d90e3d447acff77ebfc663a37608ac835459af
NatBelova <n.belova@propellerads.net>
2022-05-04 16:23:42 +0300

    fixed

--- src/dbo/StudentGroup.java
+++ src/dbo/StudentGroup.java
@@ -1,23 +1,28 @@
 package dbo;
 
-public class Student_Group {
+public class StudentGroup {
 
-    public final static String tableName = "Student group";
+    public final static String tableName = "Student_group";
 
-    public Student_Group(int id, String name, int idCurator) {
-
+    public StudentGroup(int id, String name, int idCurator, Curator curator) {
         this.id = id;
         this.name = name;
         this.idCurator = idCurator;
+        this.curator = curator;
     }
 
     private int id;
     private String name;
     private int idCurator;
+    private Curator curator;
 
     public int getId() { return id; }
 
     public String getName() { return name; }
 
     public int idCurator() { return idCurator; }
+
+    public Curator getCurator() {
+        return curator;
+    }
 }
